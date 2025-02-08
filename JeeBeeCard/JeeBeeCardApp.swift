e294e307877151915cddf4d056163596b10f5624
william farhang <farhang.william@gmail.com>
2025-02-08 04:48:58 +0100

    Initial Commit

---
+++ JeeBeeCard/JeeBeeCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeeBeeCardApp.swift
+//  JeeBeeCard
+//
+//  Created by william farhang on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct JeeBeeCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
