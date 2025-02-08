ee9db7a399eed9603b5aec5c3a44d8ee4632fff6
william farhang <farhang.william@gmail.com>
2025-02-08 07:10:00 +0100

    First prototype

---
+++ JeeBeeCard/FlashcardView.swift
@@ -0,0 +1,8 @@
+//
+//  FlashcardView.swift
+//  JeeBeeCard
+//
+//  Created by william farhang on 2025-02-08.
+//
+
+import Foundation
---
+++ JeeBeeCard/ReviewView.swift
@@ -0,0 +1,8 @@
+//
+//  ReviewView.swift
+//  JeeBeeCard
+//
+//  Created by william farhang on 2025-02-08.
+//
+
+import Foundation
