c5b4a647f290811243e2c87654da35e980bed43f
william farhang <farhang.william@gmail.com>
2025-02-09 01:36:37 +0100

    WIP: There are Navigation bugs and New deck shouln't have connected to the Swedish deck and reviews

---
+++ JeeBeeCard/DeckSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  DeckSelectionView.swift
+//  JeeBeeCard
+//
+//  Created by william farhang on 2025-02-08.
+//
+
+import SwiftUI
+
+struct DeckSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeckSelectionView()
+}
---
+++ JeeBeeCard/LandingPageView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingPageView.swift
+//  JeeBeeCard
+//
+//  Created by william farhang on 2025-02-08.
+//
+
+import SwiftUI
+
+struct LandingPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingPageView()
+}
